f8a0c6785d35786b9e4addff7b47e4b21032cdd1
Bharatchandran <bharatchandrankr@gmail.com>
2021-11-13 13:47:28 +0530

    Fresh Start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    //BEM convention
+    <div className="app">
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
